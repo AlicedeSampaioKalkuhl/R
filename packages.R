80be7ef8bb9b53d05469661217bb5a248fb62e75
AlicedeSampaioKalkuhl <alicekalkuhl@freenet.de>
2020-05-07 14:13:35 +0200

    Update packages.R

--- packages.R
+++ packages.R
@@ -3,6 +3,8 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
   BiocManager::install(version = "3.11")
 install.packages("deSolve")
+install.packages("mr.raps") #Zhao et al. 2019
 install.packages("reshape2")
 install.packages("scales")
 install.packages("tidyverse")
+install.packages("here")
